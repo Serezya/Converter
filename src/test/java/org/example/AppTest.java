279f9f1381947b0016b2c3f1d3aaf72a9dfb503e
Sergey <derbin.s-a@yandex.ru>
2020-08-01 17:02:47 +0300

    Add Unit tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,38 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import java.io.*;
 
-import org.junit.Test;
+public class AppTest {
+    @org.junit.jupiter.api.Test
+    public void existsFile() { // файл существует
+        File file = new File("data.json");
+        Assertions.assertTrue(file.exists());
+    }
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @org.junit.jupiter.api.Test
+    public void jsonNotNull() { // файл не пустой
+        File file = new File("data.json");
+        Assertions.assertNotNull(file);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void equalsFiles() throws IOException { // сравнение двух json по содержимому
+        File file = new File("data.json");
+        File file2 = new File("data2.json");
+
+        String contentFile1 = getContent(file);
+        String contentFile2 = getContent(file2);
+        Assertions.assertEquals(contentFile1, contentFile2);
+    }
+
+    public static String getContent(File file) throws IOException { // считывание файла
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        return stringBuilder.toString();
     }
 }
